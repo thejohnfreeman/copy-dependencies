988d6d737ca28a0fe1b2cde233ee7175edf82ad7
John Freeman <jfreeman08@gmail.com>
2019-03-10 19:57:45 -0500

    Solved the problem

---
+++ README.md
@@ -0,0 +1,31 @@
+# copy-dependencies
+
+As of 2019-03-10, [Eleventy](https://www.11ty.io/) does not seem to let me
+include files from `node_modules` while I'm using a custom input directory
+(which I do to scope my templates away from metadata files like `README.md`).
+I wrote this tool to let me automate the process of copying files from my
+dependencies to arbitrarily-nested paths in my project, creating any missing
+parent directories on the way (which `cp` and `rsync` alone do not do), all
+with a simple configuration that fits into `package.json`.
+
+## Install
+
+```
+yarn add -D copy-dependencies
+```
+
+## Use
+
+```js
+// package.json
+{
+  ...
+  "scripts": {
+    "install": "copy-dependencies"
+  },
+  "copyDependencies": {
+    "node_modules/prismjs/themes/prism-tomorrow.css": "src/css/theme.css"
+  },
+  ...
+}
+```
---
+++ bin.js
@@ -0,0 +1,48 @@
+const { spawn } = require('child_process')
+const path = require('path')
+const { combineLatest, concat, fromEvent } = require('rxjs')
+const { reduce, switchMap } = require('rxjs/operators')
+const { streamToRx } = require('rxjs-stream')
+
+// TODO: Take last N bytes with .slice(-N). Do we need to make a copy to free
+// the leading bytes or is Node smart enough?
+const concatBuffers = (all, next) => Buffer.concat([all, next])
+const emptyBuffer = Buffer.alloc(0)
+
+const ERROR = 0
+const WARNING = 1
+const INFO = 2
+
+function task(command, args) {
+  const subprocess = spawn(command, args)
+  const stdout = streamToRx(subprocess.stdout).pipe(
+    reduce(concatBuffers, emptyBuffer),
+  )
+  const stderr = streamToRx(subprocess.stderr).pipe(
+    reduce(concatBuffers, emptyBuffer),
+  )
+  const exit = fromEvent(subprocess, 'exit')
+  return combineLatest([exit, stdout, stderr])
+}
+
+const package = require('./package.json')
+
+if (!('copyDependencies' in package)) {
+  console.error('missing copyDependencies in package.json')
+  process.exit(1)
+}
+
+const level = ERROR
+
+Object.entries(package.copyDependencies).forEach(([from, to]) => {
+  const dir = to.endsWith(path.sep) ? to : path.dirname(to)
+  concat(
+    task('mkdir', ['--parents', dir]),
+    task('rsync', ['--recursive', from, to]),
+  ).subscribe(([[code, signal], stdout, stderr]) => {
+    if (level >= INFO || code !== 0) {
+      process.stdout.write(stdout)
+      process.stderr.write(stderr)
+    }
+  })
+})
